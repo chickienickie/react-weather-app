8f840bc97fc6648fda1def86496736ac95b7145a
chickienickie <nrs164@gmail.com>
2022-08-28 03:33:24 -0400

    updated html structure

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://nickiepotter-portfolio.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nickie Potter
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/chickienickie/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}

ff3093256cad6e193036cc9f08c910e52fc159af
chickienickie <nrs164@gmail.com>
2022-08-31 13:16:58 -0400

    added coordinates api data

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import { Axios } from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+
+  function handleResponse(response) {
+
+  }
+  const apiKey = "0dd4796646f67d5530803ad34faf15e7";
+  let lat = props.coordinates.lat;
+  let lon = props.coordinates.lon;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+
+  Axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
